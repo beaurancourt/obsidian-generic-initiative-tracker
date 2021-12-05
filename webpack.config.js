70d5f24af4268129a1da32a1e15142a25e59ff45
Beau Shinkle <beaushinkle@gmail.com>
2021-12-05 11:25:35 -0500

    Start porting over the project to test development changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,61 +5,61 @@ const sveltePreprocess = require("svelte-preprocess");
 const isDevMode = process.env.NODE_ENV === "development";
 
 module.exports = {
-    entry: "./src/main.ts",
-    output: {
-        path: path.resolve(__dirname, "."),
-        filename: "main.js",
-        libraryTarget: "commonjs"
-    },
-    target: "node",
-    mode: isDevMode ? "development" : "production",
-    ...(isDevMode ? { devtool: "eval" } : {}),
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    transpileOnly: true
-                }
-            },
-            {
-                test: /\.(svelte)$/,
-                use: [
-                    { loader: "babel-loader" },
-                    {
-                        loader: "svelte-loader",
-                        options: {
-                            preprocess: sveltePreprocess({})
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(svg|njk|html)$/,
-                type: "asset/source"
+  entry: "./src/main.ts",
+  output: {
+    path: isDevMode ? process.env.DEV_MODE_PATH : path.resolve(__dirname, "."),
+    filename: "main.js",
+    libraryTarget: "commonjs"
+  },
+  target: "node",
+  mode: isDevMode ? "development" : "production",
+  ...(isDevMode ? {devtool: "eval"} : {}),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      },
+      {
+        test: /\.(svelte)$/,
+        use: [
+          {loader: "babel-loader"},
+          {
+            loader: "svelte-loader",
+            options: {
+              preprocess: sveltePreprocess({})
             }
+          }
         ]
+      },
+      {
+        test: /\.(svg|njk|html)$/,
+        type: "asset/source"
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: "./manifest.json", to: "."},
+        {from: "./src/main.css", to: "./styles.css"}
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      svelte: path.resolve("node_modules", "svelte"),
+      "~": path.resolve(__dirname, "src"),
+      src: path.resolve(__dirname, "src")
     },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "./manifest.json", to: "." },
-                { from: "./src/main.css", to: "./styles.css" }
-            ]
-        })
-    ],
-    resolve: {
-        alias: {
-            svelte: path.resolve("node_modules", "svelte"),
-            "~": path.resolve(__dirname, "src"),
-            src: path.resolve(__dirname, "src")
-        },
-        extensions: [".ts", ".tsx", ".js", ".svelte"],
-        mainFields: ["svelte", "browser", "module", "main"]
-    },
-    externals: {
-        electron: "commonjs2 electron",
-        obsidian: "commonjs2 obsidian"
-    }
+    extensions: [".ts", ".tsx", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"]
+  },
+  externals: {
+    electron: "commonjs2 electron",
+    obsidian: "commonjs2 obsidian"
+  }
 };
